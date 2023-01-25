02f0ed613880f554e0ef2172f67036bae4de057c
Vedamir <grandros1@gmail.com>
2023-01-25 15:48:08 +0800

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int n =A;
+for(int i =1; i<B; i++)
+n=n*A;
+
+Console.Write($"A в степени B = {n} ");
