3cb75d52d121423c619029a5ff81f013ea6ae830
Vedamir <grandros1@gmail.com>
2023-01-25 23:06:15 +0800

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,7 @@
+﻿int n= 8;
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат: [{string.Join("  ", array)}]");
+}
